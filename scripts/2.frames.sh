64fc7a897b92e54f6282f2ba38ecda01c4adb9fa
Raphael Palefsky-Smith <rpalefsk@stanford.edu>
2019-05-26 21:36:35 -0400

    First pass at frame deletion

--- scripts/2.frames.sh
+++ scripts/2.frames.sh
@@ -7,7 +7,21 @@ fi
 WORKING_DIR="$BASE_DIR/temp/demo_output"
 MODULE="frames"
 
+FRAME_METHOD="opencv"
+CHANGE_THRESHOLD="1e-5"
+SEQUENTIAL_FRAMES="true"
+FPS="1"
+
+CONFIG=$( jq -n \
+	--arg m "$FRAME_METHOD" \
+	--argjson ct "$CHANGE_THRESHOLD" \
+	--argjson seq "$SEQUENTIAL_FRAMES" \
+	--argjson fps "$FPS" \
+	'{method: $m, change_threshold: $ct, sequential: $seq, fps: $fps}'
+)
+
 python3 $BASE_DIR/src/run.py \
 	--phase analyse \
 	--module $MODULE \
+	--config "$CONFIG" \
 	--folder $WORKING_DIR
