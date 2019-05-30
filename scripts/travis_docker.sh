e9dcad9218187ad33b8a955e63ebfd1bdadfcd3d
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-30 12:04:43 +0100

    push to docker hub on master CI

---
+++ scripts/travis_docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && ["$TRAVIS_BRANCH" == "master"]]; then
+	echo "CI on master branch, pushing to Docker..."
+	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD;
+	docker push $DOCKER_USERNAME/mtriage:dev;
+fi
