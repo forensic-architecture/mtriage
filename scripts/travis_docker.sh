fb8033abadf4c5fa0537bc373f585380a528b248
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-30 12:20:46 +0100

    add msg when no push

--- scripts/travis_docker.sh
+++ scripts/travis_docker.sh
@@ -4,4 +4,6 @@ if [ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && ["$TRAVIS_BRANCH" == "master"]; then
 	echo "CI on master branch, pushing to Docker..."
 	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD;
 	docker push $DOCKER_USERNAME/mtriage:dev;
+else
+	echo "Not on master branch, nothing to do."
 fi
