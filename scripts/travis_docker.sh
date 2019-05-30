367c4c036cacad087a86f745a755a858ee1fd7e2
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-30 15:44:30 +0100

    correct travis-docker script

--- scripts/travis_docker.sh
+++ scripts/travis_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && ["$TRAVIS_BRANCH" == "master"]; then
+if [ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	echo "CI on master branch, pushing to Docker..."
 	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD;
 	docker push $DOCKER_USERNAME/mtriage:dev;
