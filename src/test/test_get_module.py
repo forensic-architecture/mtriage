0b5931e04b0d01d401cfb2d1639f2e64a0135e5e
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-30 17:07:48 +0100

    add positive tests

--- src/test/test_get_module.py
+++ src/test/test_get_module.py
@@ -1,9 +1,41 @@
 from lib.common.get_module import get_module
 import unittest
+import shutil
+from os import listdir, makedirs
+from os.path import isdir
+
+def make_empty_main_export(pth):
+    INIT = "main = True\n__all__ = [\"main\"]\n"
+    with open(f"{pth}/__init__.py", "w") as f:
+        f.write(INIT)
 
 
 class TestGetModule(unittest.TestCase):
-    def test_faulty_module(self):
+    @classmethod
+    def setUpClass(self):
+        """ Make imaginary selector and analysers """
+        # tests always run from src
+        self.EMPTY_SELECTOR = "./lib/selectors/empty"
+        self.EMPTY_ANALYSER = "./lib/analysers/empty"
+
+        if isdir(self.EMPTY_SELECTOR):
+            shutil.rmtree(self.EMPTY_SELECTOR)
+        if isdir(self.EMPTY_ANALYSER):
+            shutil.rmtree(self.EMPTY_ANALYSER)
+
+        makedirs(self.EMPTY_SELECTOR)
+        make_empty_main_export(self.EMPTY_SELECTOR)
+        makedirs(self.EMPTY_ANALYSER)
+        make_empty_main_export(self.EMPTY_ANALYSER)
+
+    @classmethod
+    def tearDownClass(self):
+        if isdir(self.EMPTY_SELECTOR):
+            shutil.rmtree(self.EMPTY_SELECTOR)
+        if isdir(self.EMPTY_ANALYSER):
+            shutil.rmtree(self.EMPTY_ANALYSER)
+
+    def test_raises_when_faulty(self):
         with self.assertRaises(ModuleNotFoundError):
             get_module('selector', "smth")
 
@@ -12,4 +44,9 @@ class TestGetModule(unittest.TestCase):
 
         with self.assertRaisesRegex(ImportError,"must be 'selector' or 'analyser'"):
             get_module('neitherthing', "smth")
+
+    def test_imports_main(self):
+        # main just exported as 'True', to check import logic is correct
+        self.assertTrue(get_module('selector', 'empty'))
+        self.assertTrue(get_module('analyser', 'empty'))
 
