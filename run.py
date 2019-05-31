a362e990fc43ef1de735c6f0ad41ce5e301c8064
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-31 17:03:23 +0100

    tests: name_and_ver, should_add_pipdep

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import docker
 import inspect
+import pytest
 import os
+import re
 import argparse
 import subprocess as sp
 
@@ -17,33 +19,56 @@ DOCKER = docker.from_env()
 def get_subdirs(d):
     return [o for o in os.listdir(d) if os.path.isdir(os.path.join(d, o))]
 
+class InvalidPipDep(Exception):
+    pass
 
-def check_in_pipdeps(_str, _arr):
-    """Check whether _arr already contains a string representing the same pip dependency as _str.
+def name_and_ver(pipdep):
+    pipdep = pipdep.split("==")
+    dep_name = pipdep[0]
+    try:
+        if len(pipdep) == 1:
+            dep_version = None
+        elif len(pipdep) > 2:
+            raise InvalidPipDep
+        else:
+            dep_version = pipdep[1]
+            if re.search(r"^([0-9]{1,2}\.){0,2}([0-9]{1,2})$", dep_version) is None:
+                raise InvalidPipDep
+        return dep_name, dep_version
+    except:
+        raise InvalidPipDep
+
+def should_add_pipdep(dep, pipdeps):
+    """Check whether pipdep should be added.
     """
-    lib_name = _str.split("==")[0]
-    for depstr in _arr:
-        if depstr.find(lib_name) is not -1:
-            return True
-    return False
+    dep_name, dep_ver = name_and_ver(dep)
+    for _dep in pipdeps:
+        _dep_name, _dep_ver = name_and_ver(_dep)
+        if _dep_name == dep_name:
+            if _dep_ver is None and dep_ver is None:
+                return False
+            if _dep_ver is None:
+                return True
+            if dep_ver < _dep_ver:
+                return False
+    return True
 
 
-def check_in_dockerlines(_str, _arr):
-    """Check whether _arr already contains a string representing the same line in Dockerfile.
+def should_add_dockerline(line, dockerfile):
+    """Check whether line should be added to array representing Dockerfile.
     """
-    # NOTE: currently only exactly the same lines are disqualified
-    return _str in _arr
+    return line not in dockerfile
 
 
-def add_deps(dep_path, deps, deps_contains):
-    """ Add dependences at {folder_path} to {deps}, excluding if {deps_contains} is True for any given dependency.
+def add_deps(dep_path, deps, should_add):
+    """ Add dependences at {folder_path} to {deps}, excluding if {should_add} is True for any given dependency.
     """
     if not os.path.isfile(dep_path):
         return
 
     with open(dep_path) as f:
         for line in f.readlines():
-            if not deps_contains(line, deps):
+            if should_add(line, deps):
                 deps.append(line)
 
 
@@ -80,15 +105,15 @@ def build():
         docker_dep = "{}/{}/{}".format(SELECTORS_PATH, selector, DOCKERFILE_PARTIAL)
         pip_dep = "{}/{}/{}".format(SELECTORS_PATH, selector, PIP_PARTIAL)
 
-        add_deps(docker_dep, dockerlines, check_in_dockerlines)
-        add_deps(pip_dep, pipdeps, check_in_pipdeps)
+        add_deps(docker_dep, dockerlines, should_add_dockerline)
+        add_deps(pip_dep, pipdeps, should_add_pipdep)
 
     for analyser in analysers:
         docker_dep = "{}/{}/{}".format(ANALYSERS_PATH, analyser, DOCKERFILE_PARTIAL)
         pip_dep = "{}/{}/{}".format(ANALYSERS_PATH, analyser, PIP_PARTIAL)
 
-        add_deps(docker_dep, dockerlines, check_in_dockerlines)
-        add_deps(pip_dep, pipdeps, check_in_pipdeps)
+        add_deps(docker_dep, dockerlines, should_add_dockerline)
+        add_deps(pip_dep, pipdeps, should_add_pipdep)
 
     with open(CORE_END_DOCKER) as f:
         for line in f.readlines():
@@ -191,22 +216,11 @@ def __run_lib_tests():
         exit(returncode)
 
 def __run_runpy_tests():
-    returncode = sp.call([
-        "docker",
-        "run",
-        "--env",
-        "BASE_DIR=/mtriage",
-        # "--env-file={}".format(ENV_FILE),
-        "--rm",
-        # "--privileged",
-        "-v",
-        "{}:/mtriage".format(DIR_PATH),
-        "{}:dev".format(NAME),
-         "pytest", "test"
-    ])
+    # NOTE: runpy tests are not run in a docker container, as they operate on the local machine-- so this test is run
+    # using the LOCAL python (could be 2 or 3).
+    returncode = sp.call(["python", "-m", "pytest", "test/"])
     if returncode is 1:
         exit(returncode)
-
 
 def test():
     print("Creating container to run tests...")
--- test/test_run.py
+++ test/test_run.py
@@ -1,11 +1,48 @@
 import unittest
+import docker
+from run import name_and_ver, InvalidPipDep, should_add_pipdep
 
 
 class TestRunpy(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.stub = None
+    """ Tests the run.py file that orchestrates mtriage.
+    """
+    def test_name_and_ver(self):
+        name, ver = name_and_ver("numpy")
+        self.assertEqual(name, "numpy")
+        self.assertEqual(ver, None)
 
-    def test_run_tests(self):
-        pass
+        name, ver = name_and_ver("numpy==4.0")
+        self.assertEqual(name, "numpy")
+        self.assertEqual(ver, "4.0")
+
+        n1, v1 = name_and_ver("google-api-core==1.11.0")
+        self.assertEqual(n1, "google-api-core")
+        self.assertEqual(v1, "1.11.0")
+
+        self.assertRaises(InvalidPipDep, name_and_ver, "numpy==")
+        self.assertRaises(InvalidPipDep, name_and_ver, "invalid==2.h")
+        self.assertRaises(InvalidPipDep, name_and_ver, "invalid==2==")
+
+    def test_should_add_pipdeps(self):
+        p1 = []
+        # empty check --> false
+        self.assertTrue(should_add_pipdep("numpy", p1))
+
+        p2 = ["numpy"]
+        self.assertFalse(should_add_pipdep("numpy", p2))
+        self.assertTrue(should_add_pipdep("pandas", p2))
+        # should add specific versions over undefined
+        self.assertTrue(should_add_pipdep("numpy==2.0", p2))
+        # should add higher versions
+        p3 = ["numpy==1.0"]
+        self.assertTrue(should_add_pipdep("numpy==3.0", p3))
+        # check with multiple
+        p4 = ["pack1==2.0", "pandas=3.4", "numpy==1.0", "blueray"]
+        self.assertTrue(should_add_pipdep("numpy==1.1", p4))
+        self.assertFalse(should_add_pipdep("numpy", p4))
+        self.assertTrue(should_add_pipdep("blueray==0.1", p4))
+        self.assertTrue(should_add_pipdep("newdep", p4))
+        # check error
+        with self.assertRaises(InvalidPipDep):
+            should_add_pipdep("invalid==1==", p4)
 
