b95419b3a87c005330a5d4387b2956005c142cfd
Lachlan Kermode <lachiekermode@gmail.com>
2019-06-04 10:26:48 +0100

    mv test folder to src

---
+++ src/test/test_entry.py
@@ -0,0 +1,14 @@
+from run import _analyse_run, _select_run
+import os
+import unittest
+
+
+class TestEntry(unittest.TestCase):
+    # def setUp(self):
+    #     self.LOGPATH = "testlog.txt"
+    #
+    # def tearDown(self):
+    #     if os.path.exists(self.LOGPATH):
+    #         os.remove(self.LOGPATH)
+    def test_analyse_run(self):
+        self.assertTrue(False)
