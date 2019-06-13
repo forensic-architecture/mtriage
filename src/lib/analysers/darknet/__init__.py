bbbd117a00df5fb12e7968f1e8c6f5f0f68ec51a
Lachlan Kermode <lachiekermode@gmail.com>
2019-06-13 17:15:25 +0100

    rm stray darknet analyser

--- src/lib/analysers/darknet/__init__.py
+++
@@ -1,5 +0,0 @@
-"""The DARKNET module is an ANALYSER that produces only single images.
-"""
-from .main import DarknetAnalyser as main
-
-__all__ = ["main"]
--- src/lib/analysers/darknet/main.py
+++
@@ -1,7 +0,0 @@
-from lib.common.analyser import Analyser
-
-
-class DarknetAnalyser(Analyser):
-    def analyse_element(self, element, config):
-        # TODO: this analyser is a stub currently.
-        print(element)
