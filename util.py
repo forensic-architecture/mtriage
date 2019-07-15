73de6b9015c5e46ccb7b4bda4fba03b2c007e858
Lachlan Kermode <lachiekermode@gmail.com>
2019-07-15 11:25:41 +0100

    allow multiple mtriage processes to run at once

--- util.py
+++ util.py
@@ -153,12 +153,13 @@ def create_server_config(configPath, configDict):
     with open(configPath, "w") as config:
         json.dump(configDict, config)
 
+
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no','false','f','n','0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
         raise argparse.ArgumentTypeError("Boolean value expected.")
