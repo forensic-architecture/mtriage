85b6729d4db96d8ad141d0d10407bc254381b3a3
Lachlan Kermode <lachiekermode@gmail.com>
2019-08-06 18:00:26 +0100

    strip to simple module-wise tests

--- src/conftest.py
+++ src/conftest.py
@@ -6,27 +6,29 @@ import requests
 import os
 import test.utils as test_utils
 
-EG_VIDEO = "https://datasheet-sources.ams3.digitaloceanspaces.com/ilovaisk_videos/platform_background.mp4"
-EG_IMAGE = "https://datasheet-sources.ams3.digitaloceanspaces.com/ilovaisk_videos/Platform_Tutorial_thumb.png"
-
 
 @pytest.fixture(scope="session", autouse=True)
 def test_element_dir():
     return "../temp/test"
 
 
-@pytest.fixture(scope="session", autouse=True)
-def stubs():
-    if not os.path.exists("/test"):
-        os.makedirs("/test")
-    if not os.path.exists("/test/video.mp4"):
-        r = requests.get(EG_VIDEO)
-        open("/test/video.mp4", "wb").write(r.content)
-    if not os.path.exists("/test/image.png"):
-        r = requests.get(EG_IMAGE)
-        open("/test/image.png", "wb").write(r.content)
+# TODO(lachlan): create a special fixture to allow component-wise tests to analyse sub elements
+# EG_VIDEO = "https://datasheet-sources.ams3.digitaloceanspaces.com/ilovaisk_videos/platform_background.mp4"
+# EG_IMAGE = "https://datasheet-sources.ams3.digitaloceanspaces.com/ilovaisk_videos/Platform_Tutorial_thumb.png"
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def analyse_stub_element()
+#     if not os.path.exists("/test"):
+#         os.makedirs("/test")
+#     if not os.path.exists("/test/video.mp4"):
+#         r = requests.get(EG_VIDEO)
+#         open("/test/video.mp4", "wb").write(r.content)
+#     if not os.path.exists("/test/image.png"):
+#         r = requests.get(EG_IMAGE)
+#         open("/test/image.png", "wb").write(r.content)
+#
+#     return "some val"
 
-    return "some val"
 
 @pytest.fixture(scope="session", autouse=True)
 def utils():
--- src/lib/analysers/frames/test/test_frames.py
+++ src/lib/analysers/frames/test/test_frames.py
@@ -1,7 +1,5 @@
 import pytest
 from lib.analysers.frames import main as frames
 
-def test_frames(stubs):
-    print(frames)
-    print(stubs)
+def test_frames():
     assert 5 == 5
