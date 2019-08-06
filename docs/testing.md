db8cd2a4146a0b6c0f990e649006e147c1f59ae9
Lachlan Kermode <lachiekermode@gmail.com>
2019-08-06 18:13:01 +0100

    fmt

---
+++ docs/testing.md
@@ -0,0 +1,18 @@
+# Testing
+
+Mtriage has three kinds of tests:
+1. Tests for the core code that runs inside Docker (in src/test).
+2. Tests for the outer orchestration logic (in test/).
+3. Tests for analysers and selectors (in each component folder, in test/).
+
+Each kind of test is run with appropriate containerisation given its context,
+i.e. tests of type 1 are run inside Docker, whereas tests of type 2 are run
+using the locally installed Python environment.
+
+To run all tests, use the following command:
+```
+./mtriage run test
+```
+
+See docs/custom-components.md for more information on how to write tests for
+a particular component.
--- src/test/test_selector.py
+++ src/test/test_selector.py
@@ -31,20 +31,29 @@ def additionals(utils):
     yield obj
     utils.cleanup()
 
+
 def test_selector_imports():
     assert type(Selector) == type(ABC)
+
 
 def test_cannot_instantiate(utils):
     with pytest.raises(TypeError):
         Selector({}, "empty", utils.TEMP_ELEMENT_DIR)
 
+
 def test_init(utils, additionals):
     assert utils.TEMP_ELEMENT_DIR == additionals.emptySelector.BASE_DIR
     assert "empty" == additionals.emptySelector.NAME
     assert f"{utils.TEMP_ELEMENT_DIR}/empty" == additionals.emptySelector.DIR
-    assert f"{utils.TEMP_ELEMENT_DIR}/empty/data" == additionals.emptySelector.ELEMENT_DIR
-    assert f"{utils.TEMP_ELEMENT_DIR}/empty/element_map.csv" == additionals.emptySelector.ELEMENT_MAP
+    assert (
+        f"{utils.TEMP_ELEMENT_DIR}/empty/data" == additionals.emptySelector.ELEMENT_DIR
+    )
+    assert (
+        f"{utils.TEMP_ELEMENT_DIR}/empty/element_map.csv"
+        == additionals.emptySelector.ELEMENT_MAP
+    )
     assert os.path.exists(additionals.emptySelector.ELEMENT_DIR)
+
 
 def test_index(additionals):
     additionals.emptySelector.start_indexing()
@@ -54,6 +63,7 @@ def test_index(additionals):
         emreader = csv.reader(f, delimiter=",")
         rows = [l for l in emreader]
         assert rows == scaffold_elementmap(["el1", "el2", "el3"])
+
 
 # NOTE: not sure why this stopped working with refactor to pytest
 # def test_start_retrieving(utils, additionals):
