3970a47d7ba00016c42f3c9a75c05e24c5607e22
Lachlan Kermode <lachiekermode@gmail.com>
2019-10-30 13:34:19 +0000

    allow hashing method to be passed via config

--- src/lib/analysers/imagededup/main.py
+++ src/lib/analysers/imagededup/main.py
@@ -1,7 +1,8 @@
 import json
 from pathlib import Path
 from shutil import copyfile
-from imagededup.methods import PHash
+from imagededup import methods
+from lib.common.exceptions import InvalidAnalyserConfigError
 from lib.common.analyser import Analyser
 from lib.common.etypes import Etype
 
@@ -13,20 +14,40 @@ class ImagededupAnalyser(Analyser):
     def get_out_etype(self):
         return Etype.AnnotatedImageArray
 
+    def __create_hasher(self, config):
+        hasher_key = config["method"] if "method" in config else "phash"
+        self.logger(f"Compare method is {hasher_key}")
+        hasher = {
+            "phash": methods.PHash,
+            "ahash": methods.AHash,
+            "dhash": methods.DHash,
+            "whash": methods.WHash,
+        }.get(hasher_key)
+        if hasher is None:
+            raise InvalidAnalyserConfigError(f"'{hasher_key}' is not a valid method for imagededup.")
+
+        self.hasher = hasher()
+
+        # super low threshold by default to only remove essentially identical images.
+        if "threshold" in config:
+            self.threshold = int(config["threshold"])
+        else:
+            self.threshold = 3
+
+        self.logger(f"Hamming threshold is {self.threshold}")
+
     def pre_analyse(self, config):
-        self.phasher = PHash()
+        self.__create_hasher(config)
 
     def analyse_element(self, element, config):
         dest = element["dest"]
         basedir = element["base"]
-        # super low threshold by default to only remove essentially identical images.
-        threshold = int(config["threshold"]) if "threshold" in config else 2
-        self.logger(f"Hamming threshold is {threshold}")
 
-        encodings = self.phasher.encode_images(image_dir=basedir)
-        duplicates = self.phasher.find_duplicates_to_remove(
-            image_dir=basedir, max_distance_threshold=threshold
-        )
+        encodings = self.hasher.encode_images(image_dir=basedir)
+
+        args = {"image_dir": basedir, "max_distance_threshold": self.threshold}
+
+        duplicates = self.hasher.find_duplicates_to_remove(**args)
 
         self.logger(
             f"{len(duplicates)} duplicates found, copying over all other files..."
