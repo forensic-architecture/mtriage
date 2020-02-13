618f271bf612142a56ab385f489d086092523f2a
James Bradbury <jamesbradbury93@gmail.com>
2020-02-13 12:01:57 +0000

    working prototype for downloading images

--- src/lib/selectors/4chan/4chanGET.py
+++ src/lib/selectors/4chan/4chanGET.py
@@ -1,29 +1,54 @@
-import requests
-import json
+import requests, json, time, os
 
 # Should be a board > thread > post > content
 
+# parameters:
+"""
+--search-term
+--pages?
+--age
+--replies
+--structured or dump files (files go into a structured folder hierarchy)
+"""
 
-boards_requests = "https://a.4cdn.org/boards.json"
-archive_requests = "https://a.4cdn.org/po/archive.json"
+MIN_REPLIES = 1
+
+base_dir = "/Users/james/4chanscraping"
+
+board_choice = "pol"
+
+req = f"https://a.4cdn.org/{board_choice}/threads.json"
 
 #-- Various requests --#
 # Boards
-boards_response = requests.get(boards_requests)
-boards_content = json.loads(boards_response.content)
+content = json.loads(
+    requests.get(req).content
+)
+# https://a.4cdn.org/po/thread/570368.json
+for page in content:
+    for threads in page["threads"]:
+        if threads["replies"] >= MIN_REPLIES:
+            thread_id = threads["no"]
+            req = f"https://a.4cdn.org/{board_choice}/thread/{thread_id}.json"
+            thread_content = json.loads(requests.get(req).content)["posts"]
+            # thread content is a list of posts
+            for content in thread_content:
+                # now operate on a post
 
-# Archive
-archive_response = requests.get(archive_requests)
-archive_content = json.loads(archive_response.content)
-
-# You should be to ask for a certain media type, text and then some filters
-
-# Get boards
-# for board in content["boards"]:
-#     print(board["title"])
-
-# Get archive
-# Returns you a list of thread ID's which have to be later parsed
-print(archive_content)
-
-
+                try:
+                    filename = content['filename']
+                except KeyError:
+                    print("No file for that ")
+                else:
+                    content_id = content['no']
+                    time_id = content['tim']
+                    comment = content['com'] #strip HTML from this
+                    extension = content['ext']
+                    full_file = f"{filename}{extension}"
+                    # https://i.4cdn.org/[board]/[4chan image ID].[file extension]
+                    file_url = f"https://i.4cdn.org/{board_choice}/{time_id}{extension}"
+                    img_data = requests.get(file_url).content
+                    out_path = os.path.join(base_dir, full_file)
+                    with open(out_path, 'wb') as handler:
+                        handler.write(img_data)
+                    time.sleep(1)
