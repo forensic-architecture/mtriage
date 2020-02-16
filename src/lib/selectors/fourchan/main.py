eab356daa7cde21c70278ea361c47f74e2c3389a
James Bradbury <jamesbradbury93@gmail.com>
2020-02-16 01:28:27 +0000

    black formatting

--- src/lib/selectors/fourchan/main.py
+++ src/lib/selectors/fourchan/main.py
@@ -13,78 +13,134 @@ class FourChanSelector(Selector):
 
     def index(self, config):
         viable_boards = [
-            "a","aco","adv","an","asp",
-            "b","bant","biz",
-            "c","cgl","ck","cm","co",
-            "d","diy",
+            "a",
+            "aco",
+            "adv",
+            "an",
+            "asp",
+            "b",
+            "bant",
+            "biz",
+            "c",
+            "cgl",
+            "ck",
+            "cm",
+            "co",
+            "d",
+            "diy",
             "e",
-            "f","fa","fit",
-            "g","gd","gif",
-            "h","hc","his","hm","hr",
-            "i","ic","int",
+            "f",
+            "fa",
+            "fit",
+            "g",
+            "gd",
+            "gif",
+            "h",
+            "hc",
+            "his",
+            "hm",
+            "hr",
+            "i",
+            "ic",
+            "int",
             "jp",
             "k",
-            "lgbt","lit",
-            "m","mlp","mu",
-            "n","news",
-            "o","out",
-            "p","po","pol",
-            "qa","qst",
-            "r","r9k",
-            "s","s4s","sci","soc","sp",
-            "t","tg","toy","trash","trv","tv",
+            "lgbt",
+            "lit",
+            "m",
+            "mlp",
+            "mu",
+            "n",
+            "news",
+            "o",
+            "out",
+            "p",
+            "po",
+            "pol",
+            "qa",
+            "qst",
+            "r",
+            "r9k",
+            "s",
+            "s4s",
+            "sci",
+            "soc",
+            "sp",
+            "t",
+            "tg",
+            "toy",
+            "trash",
+            "trv",
+            "tv",
             "u",
-            "v","vg","vip","vp","vr",
-            "w","wg","wsg","wsr",
+            "v",
+            "vg",
+            "vip",
+            "vp",
+            "vr",
+            "w",
+            "wg",
+            "wsg",
+            "wsr",
             "x",
-            "y"
-        ]   
+            "y",
+        ]
         results = []
         board = config["board"]
         if board not in viable_boards:
-            self.error_logger('Your chosen board does not exist on 4chan!')
+            self.error_logger("Your chosen board does not exist on 4chan!")
             quit()
         max_posts = config["max_posts"]
         # Create a HTML parser for parsing comments
         h = html2text.HTML2Text()
         h.ignore_links = False
-        
+
         req = f"https://a.4cdn.org/{board}/threads.json"
 
-        content = json.loads(
-            requests.get(req).content
-        )
+        content = json.loads(requests.get(req).content)
         page_count = 0
         for page_index, page in enumerate(content):
             if page_index < max_posts:
                 self.logger(f"Scraping page number: {page_index+1}")
                 for thread_index, threads in enumerate(page["threads"]):
-                    self.logger(f"Extracting posts from thread number: {thread_index+1}")
+                    self.logger(
+                        f"Extracting posts from thread number: {thread_index+1}"
+                    )
                     thread_id = threads["no"]
                     req = f"https://a.4cdn.org/{board}/thread/{thread_id}.json"
-                    thread_content = json.loads(requests.get(req).content)["posts"] # thread content is a list of posts
+                    thread_content = json.loads(requests.get(req).content)[
+                        "posts"
+                    ]  # thread content is a list of posts
                     for post_index, post in enumerate(thread_content):
-                        self.logger(f"Extracting media and comments from post number: {post_index+1}")
+                        self.logger(
+                            f"Extracting media and comments from post number: {post_index+1}"
+                        )
                         post_row = []
-                        post_row.append(post['no'])
+                        post_row.append(post["no"])
                         post_row.append(thread_id)
-                        post_row.append(post['time'])
-                        
-                        try: comment = post['com']
-                        except KeyError: comment = "..."
+                        post_row.append(post["time"])
+
+                        try:
+                            comment = post["com"]
+                        except KeyError:
+                            comment = "..."
                         else:
                             comment = h.handle(comment)
                         post_row.append(comment)
 
                         # Filename
-                        try: filename = post['filename']
-                        except KeyError: filename = ""
+                        try:
+                            filename = post["filename"]
+                        except KeyError:
+                            filename = ""
 
                         if filename != "":
-                            time_id = post['tim']
-                            extension = post['ext']
+                            time_id = post["tim"]
+                            extension = post["ext"]
                             full_file = f"{filename}{extension}"
-                            file_url = f"https://i.4cdn.org/{board}/{time_id}{extension}"
+                            file_url = (
+                                f"https://i.4cdn.org/{board}/{time_id}{extension}"
+                            )
                             post_row.append(full_file)
                             post_row.append(extension)
                             post_row.append(file_url)
@@ -92,10 +148,12 @@ class FourChanSelector(Selector):
                             post_row.append("")
                             post_row.append("")
                             post_row.append("")
-                        
+
                         results.append(post_row)
-        self.logger('Scraping metadata complete')
-        results.insert(0, ["id", "thread_id", "datetime", "comment", "filename", "ext", "url"])
+        self.logger("Scraping metadata complete")
+        results.insert(
+            0, ["id", "thread_id", "datetime", "comment", "filename", "ext", "url"]
+        )
         return results
 
     def retrieve_element(self, element, config):
@@ -104,12 +162,9 @@ class FourChanSelector(Selector):
         comment = element["comment"]
         dest = element["base"]
         url = element["url"]
-        
-        with open(os.path.join(dest, f'{identifier}_comment.txt'), "w+") as f:
+
+        with open(os.path.join(dest, f"{identifier}_comment.txt"), "w+") as f:
             f.write(comment)
-        
+
         if url != "":
-            urlretrieve(
-                url,
-                os.path.join(dest, fn)
-            )
+            urlretrieve(url, os.path.join(dest, fn))
