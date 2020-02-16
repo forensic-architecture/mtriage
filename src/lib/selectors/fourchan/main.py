01e2767f449ee163d9fb08d618e85753901868d4
James Bradbury <jamesbradbury93@gmail.com>
2020-02-16 00:00:17 +0000

    logging and comment parsing

--- src/lib/selectors/fourchan/main.py
+++ src/lib/selectors/fourchan/main.py
@@ -1,5 +1,5 @@
 import json, requests, re, os
-from html.parser import HTMLParser
+import html2text
 from urllib.request import urlretrieve
 from lib.common.selector import Selector
 from lib.common.etypes import Etype
@@ -15,33 +15,37 @@ class fourchan(Selector):
         results = []
         board = config["board"]
         max_posts = config["max_posts"]
-        #TODO: add thread id
+        # Create a HTML parser for parsing comments
+        h = html2text.HTML2Text()
+        h.ignore_links = False
         
         req = f"https://a.4cdn.org/{board}/threads.json"
 
-        #-- Various requests --#
-        # Boards
         content = json.loads(
             requests.get(req).content
         )
-        post_count = 0
-        for page in content:
-            if post_count < max_posts:
-                post_count += 1
-                for threads in page["threads"]:
+        page_count = 0
+        for page_index, page in enumerate(content):
+            if page_index < max_posts:
+                self.logger(f"Scraping page number: {page_index+1}")
+                # post_count += 1
+                for thread_index, threads in enumerate(page["threads"]):
+                    self.logger(f"Extracting posts from thread number: {thread_index+1}")
                     thread_id = threads["no"]
                     req = f"https://a.4cdn.org/{board}/thread/{thread_id}.json"
                     thread_content = json.loads(requests.get(req).content)["posts"] # thread content is a list of posts
-                    for post in thread_content:
+                    for post_index, post in enumerate(thread_content):
+                        self.logger(f"Extracting media and comments from post number: {post_index+1}")
                         post_row = []
-
                         post_row.append(post['no'])
                         post_row.append(post['time'])
                         
-                        # Comment
-                        try: comment = re.sub('<[^<]+?>', '', post['com']) # TODO: regex removes _some_ HTML. A better solution is required
+                        try: comment = post['com']
                         except KeyError: comment = "..."
+                        else:
+                            comment = h.handle(comment)
                         post_row.append(comment)
+
 
                         # Filename
                         try: filename = post['filename']
@@ -61,6 +65,7 @@ class fourchan(Selector):
                             post_row.append("")
                         
                         results.append(post_row)
+        self.logger('Scraping metadata complete')
         results.insert(0, ["id", "datetime", "comment", "filename", "ext", "url"])
         return results
 
