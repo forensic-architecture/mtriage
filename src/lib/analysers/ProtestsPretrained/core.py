5d13ccd1650885517bc384b912ddb996e08061c6
Jules Simon <jules.simon@protonmail.com>
2020-03-25 14:01:06 -0400

    Adding prostests analyser

---
+++ src/lib/analysers/ProtestsPretrained/core.py
@@ -0,0 +1,71 @@
+import numpy as np
+import json
+import os
+import torch
+from torch.autograd import Variable
+from PIL import Image
+from utils import transform, modified_resnet50, decode
+
+from importlib import import_module
+from lib.common.exceptions import InvalidAnalyserConfigError
+from lib.common.analyser import Analyser
+from lib.common.etypes import Etype, Union, Array
+
+from utils import transform, modified_resnet50
+
+# TODO cuda ?
+
+
+class ProtestsPretrained(Analyser):
+    def pre_analyse(self, config):
+        """
+        Init the logging, etc
+        Init the model
+        Same as KerasPretrained
+        """
+        rLabels = config["labels"]
+        self.THRESH = 0.1
+
+        t = transform()
+        model = modified_resnet50()
+        model.load_state_dict(
+            torch.load("model.pth.tar", map_location=torch.device("cpu"))[
+                "state_dict"
+            ]
+        )
+        model.eval()
+
+        def get_preds(img_path):
+            """
+            Gives labelds and probabilities for a single image
+            This is were we preprocess the image, using a function defined in the model class
+            """
+            # load image
+            img = Image.open(img_path).convert("RGB")
+            # process it
+            x = t(img)
+            # get in in the right format
+            x = Variable(x).unsqueeze(0)
+            # predictions
+            output = model(x)
+            # decode
+            output = decode(output.cpu().data.numpy()[0])
+            # filter
+            output = [(x[0], x[1]) for x in output if x[0] in rLabels]
+            output = [(x[0], x[1]) for x in output if x[1] >= self.THRESH]
+
+            return output
+
+        self.get_preds = get_preds
+
+        def analyse_element(
+            self, element: Union(Array(Etype.Image), Etype.Json), _
+        ) -> Etype.Json:
+            self.logger(f"Running inference on frames in {element.id}...")
+            val = Etype.CvJson.from_preds(element, self.get_preds)
+            self.logger(f"Wrote predictions JSON for {element.id}.")
+            self.disk.delete_local_on_write = True
+            return val
+
+
+module = ProtestsPretrained
---
+++ src/lib/analysers/ProtestsPretrained/test.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+import torch
+from torch.autograd import Variable
+from PIL import Image
+from utils import transform, modified_resnet50, decode
+
+
+def pre_analyse():
+    """
+    Init the logging, etc
+    Init the model
+    Same as KerasPretrained
+    """
+    t = transform()
+    model = modified_resnet50()
+    model.load_state_dict(
+        torch.load("model.pth.tar",
+            map_location=torch.device("cpu"),
+        )["state_dict"]
+    )
+    model.eval()
+
+    def get_preds(img_path):
+        """
+        Gives labelds and probabilities for a single image
+        This is were we preprocess the image, using a function defined in the model class
+        """
+        # load image
+        img = Image.open(img_path).convert("RGB")
+        # process it
+        x = t(img)
+        # get in in the right format
+        x = Variable(x).unsqueeze(0)
+        # predictions
+        output = model(x)
+        # decode
+        output = decode(output.cpu().data.numpy()[0])
+
+        # filter
+        # return pred, proba
+        return output
+
+    return get_preds("image.jpg")
+
+
+print(pre_analyse())
