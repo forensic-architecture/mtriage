58526692ad5a2ba355e9b762762de59aa71adea7
Jules Simon <jules.simon@protonmail.com>
2020-04-01 15:26:53 -0400

    Fixed syntax errors in .yaml, moved utils.py into core.

--- src/lib/analysers/ProtestsPretrained/core.py
+++ src/lib/analysers/ProtestsPretrained/core.py
@@ -4,14 +4,66 @@ import os
 import torch
 from torch.autograd import Variable
 from PIL import Image
-from utils import transform, modified_resnet50, decode
+#import sys
+#from utils import transform, modified_resnet50, decode
 
 from lib.common.analyser import Analyser
 from lib.common.etypes import Etype, Union, Array
 
-from utils import transform, modified_resnet50
-
 # TODO cuda ?
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+
+def transform():
+    return transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+
+def decode(preds):
+    classes = [
+        "protest",
+        "violence",
+        "sign",
+        "photo",
+        "fire",
+        "police",
+        "children",
+        "group_20",
+        "group_100",
+        "flag",
+        "night",
+        "shouting",
+    ]
+    return [(x, preds[c]) for c, x in enumerate(classes)]
+
+
+class FinalLayer(nn.Module):
+    """modified last layer for resnet50 for our dataset"""
+
+    def __init__(self):
+        super(FinalLayer, self).__init__()
+        self.fc = nn.Linear(2048, 12)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.fc(x)
+        out = self.sigmoid(out)
+        return out
+
+
+def modified_resnet50():
+    model = models.resnet50(pretrained=True)
+    model.fc = FinalLayer()
+    return model
 
 
 class ProtestsPretrained(Analyser):
@@ -19,10 +71,9 @@ class ProtestsPretrained(Analyser):
         """
         Init the logging, etc
         Init the model
-        Same as KerasPretrained
         """
         rLabels = config["labels"]
-        self.THRESH = 0.1
+        self.THRESH = 0.6
 
         t = transform()
         model = modified_resnet50()
@@ -54,14 +105,14 @@ class ProtestsPretrained(Analyser):
 
         self.get_preds = get_preds
 
-        def analyse_element(
-            self, element: Union(Array(Etype.Image), Etype.Json), _
-        ) -> Etype.Json:
-            self.logger(f"Running inference on frames in {element.id}...")
-            val = Etype.CvJson.from_preds(element, self.get_preds)
-            self.logger(f"Wrote predictions JSON for {element.id}.")
-            self.disk.delete_local_on_write = True
-            return val
+    def analyse_element(
+        self, element: Union(Array(Etype.Image), Etype.Json), _
+    ) -> Etype.Json:
+        self.logger(f"Running inference on frames in {element.id}...")
+        val = Etype.CvJson.from_preds(element, self.get_preds)
+        self.logger(f"Wrote predictions JSON for {element.id}.")
+        self.disk.delete_local_on_write = True
+        return val
 
 
 module = ProtestsPretrained
