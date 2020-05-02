f70372fbeb3cbea3de29fc0b8902321e8acf9648
James Bradbury <jamesbradbury93@gmail.com>
2020-05-03 00:54:17 +0100

    remove limitation parameter

--- src/lib/selectors/fourchan/core.py
+++ src/lib/selectors/fourchan/core.py
@@ -96,7 +96,6 @@ class FourChan(Selector):
         if board not in viable_boards:
             self.error_logger("Your chosen board does not exist on 4chan!")
             quit()
-        max_posts = int(config["max_posts"])
         # Create a HTML parser for parsing comments
         h = html2text.HTML2Text()
         h.ignore_links = False
@@ -104,58 +103,55 @@ class FourChan(Selector):
         req = f"https://a.4cdn.org/{board}/threads.json"
 
         content = json.loads(requests.get(req).content)
-        page_count = 0
         for page_index, page in enumerate(content):
-            if page_index < max_posts:
-                self.logger(f"Scraping page number: {page_index+1}")
-                for thread_index, threads in enumerate(page["threads"]):
+            self.logger(f"Scraping page number: {page_index+1}")
+            for thread_index, threads in enumerate(page["threads"]):
+                self.logger(
+                    f"Extracting posts from thread number: {thread_index+1}"
+                )
+                thread_id = threads["no"]
+                req = f"https://a.4cdn.org/{board}/thread/{thread_id}.json"
+                thread_content = json.loads(requests.get(req).content)[
+                    "posts"
+                ]  # thread content is a list of posts
+                for post_index, post in enumerate(thread_content):
                     self.logger(
-                        f"Extracting posts from thread number: {thread_index+1}"
+                        f"Extracting media and comments from post number: {post_index+1}"
                     )
-                    thread_id = threads["no"]
-                    req = f"https://a.4cdn.org/{board}/thread/{thread_id}.json"
-                    thread_content = json.loads(requests.get(req).content)[
-                        "posts"
-                    ]  # thread content is a list of posts
-                    for post_index, post in enumerate(thread_content):
-                        self.logger(
-                            f"Extracting media and comments from post number: {post_index+1}"
+                    post_row = []
+                    post_row.append(post["no"])
+                    post_row.append(thread_id)
+                    post_row.append(post["time"])
+
+                    try:
+                        comment = post["com"]
+                    except KeyError:
+                        comment = "..."
+                    else:
+                        comment = h.handle(comment)
+                    post_row.append(comment)
+
+                    # Filename
+                    try:
+                        filename = post["filename"]
+                    except KeyError:
+                        filename = ""
+
+                    if filename != "":
+                        time_id = post["tim"]
+                        extension = post["ext"]
+                        full_file = f"{filename}{extension}"
+                        file_url = (
+                            f"https://i.4cdn.org/{board}/{time_id}{extension}"
                         )
-                        post_row = []
-                        post_row.append(post["no"])
-                        post_row.append(thread_id)
-                        post_row.append(post["time"])
-
-                        try:
-                            comment = post["com"]
-                        except KeyError:
-                            comment = "..."
-                        else:
-                            comment = h.handle(comment)
-                        post_row.append(comment)
-
-                        # Filename
-                        try:
-                            filename = post["filename"]
-                        except KeyError:
-                            filename = ""
-
-                        if filename != "":
-                            time_id = post["tim"]
-                            extension = post["ext"]
-                            full_file = f"{filename}{extension}"
-                            file_url = (
-                                f"https://i.4cdn.org/{board}/{time_id}{extension}"
-                            )
-                            post_row.append(full_file)
-                            post_row.append(extension)
-                            post_row.append(file_url)
-                        elif filename == "":
-                            post_row.append("")
-                            post_row.append("")
-                            post_row.append("")
-
-                        results.append(post_row)
+                        post_row.append(full_file)
+                        post_row.append(extension)
+                        post_row.append(file_url)
+                    elif filename == "":
+                        post_row.append("")
+                        post_row.append("")
+                        post_row.append("")
+                    results.append(post_row)
         self.logger("Scraping metadata complete")
         results.insert(
             0, ["id", "thread_id", "datetime", "comment", "filename", "ext", "url"]
