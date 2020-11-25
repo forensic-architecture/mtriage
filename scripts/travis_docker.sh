d33b42a4e272597d09be9b8e422ae66e894f9f7b
Lachlan Kermode <lachiekermode@gmail.com>
2020-11-25 14:10:31 +0000

    update travis for new branch structure

--- scripts/travis_docker.sh
+++ scripts/travis_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	echo "CI on master branch, pushing to Docker..."
+if [ ! "$TRAVIS_PULL_REQUEST_BRANCH" ] && [ "$TRAVIS_BRANCH" == "main" ]; then
+	echo "CI on main branch, pushing to Docker..."
 	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD;
 	docker push $DOCKER_USERNAME/mtriage:dev;
 	docker push $DOCKER_USERNAME/mtriage:dev-gpu;
 else
-	echo "Not on master branch, nothing to do."
+	echo "Not on main branch, nothing to do."
 fi
