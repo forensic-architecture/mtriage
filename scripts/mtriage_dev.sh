87b4a4ea7a025e759b47255c4ad246e552c92ab1
Lachlan Kermode <lachiekermode@gmail.com>
2021-02-10 06:32:22 +0100

    Topic/scaffold scripts (#177)
    * rm yolov5; setup scaffold script
    
    * add basic analyser scaffolder
    
    * rm travis
    
    Co-authored-by: Lachlan <Kermode>

--- scripts/mtriage_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# run inside a bash shell in the Docker container, from mtriage directory using:
-# 	./scripts/mtriage_dev examples/an-frames.yaml
-
-cp $1 /run_args.yaml
-python src/run.py
-rm /run_args.yaml
